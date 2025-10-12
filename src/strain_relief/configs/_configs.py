a6d0f63bbeac7b3fe4d39451a7ff8e736cc8fe9a
Ewan Wallace <ewan.wallace@roche.com>
2025-10-12 18:55:29 -0400

    updated to match new config structure

--- src/strain_relief/configs/_configs.py
+++ src/strain_relief/configs/_configs.py
@@ -12,22 +12,32 @@ class ExperimentConfigurationError(Exception):
 
 
 def _validate_model(cfg: DictConfig):
-    if (
-        cfg.local_min.method in ["MACE", "FAIRChem"]
-        or cfg.global_min.method in ["MACE", "FAIRChem"]
-        or cfg.energy_eval.method in ["MACE", "FAIRChem"]
-    ):
-        if cfg.model.model_paths is None:
-            raise ExperimentConfigurationError("Model path must be provided if using a NNP")
+    """Ensures model paths is provdied if a NNP model is being used."""
+    if cfg.calculator.model_paths is None:
+        raise ExperimentConfigurationError("Model path must be provided if using a NNP")
 
-        if not Path(cfg.model.model_paths).exists():
-            raise ExperimentConfigurationError(f"Model path {cfg.model.model_paths} does not exist")
+    if not Path(cfg.calculator.model_paths).exists():
+        raise ExperimentConfigurationError(f"Model path {cfg.model.model_paths} does not exist")
+
+    if cfg.get("energy_evaluation", None):
+        if cfg.energy_evaluation.calculator.model_paths is None:
+            raise ExperimentConfigurationError("Model path must be provided if using a NNP")
+        elif not Path(cfg.energy_evaluation.calculator.model_paths).exists():
+            raise ExperimentConfigurationError(
+                f"Model path {cfg.energy_evaluation.model_paths} does not exist"
+            )
+
+
+def _validate_batch(cfg: DictConfig) -> DictConfig:
+    """Validate the batch size and set to a large number if -1 (i.e. no batching)."""
+    if cfg.batch_size == -1:
+        cfg.batch_size = 1_000_000
+    return cfg
 
 
 def _validate_config(cfg: DictConfig) -> DictConfig:
-    """
-    Validate the config and make any necessary alterations to the parameters.
-    """
+    """Validate the config and make any necessary alterations to the parameters."""
     _validate_model(cfg)
+    cfg = _validate_batch(cfg)
 
     return cfg
