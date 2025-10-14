323aca1357d528a922267c99946500afacedd634
Ewan Wallace <ewan.wallace@roche.com>
2025-10-14 09:27:23 -0700

    work in progress

--- tests/test_compute_strain.py
+++ tests/test_compute_strain.py
@@ -10,14 +10,15 @@ from strain_relief.io import load_parquet
 @pytest.mark.parametrize("eval_method", ["mmff94", "mmff94s"])
 @pytest.mark.parametrize("min_method", ["mmff94", "mmff94s"])
 def test_strain_relief(min_method: str, eval_method: str):
-    with initialize(version_base="1.1", config_path="../src/strain_relief/hydra_config"):
+    with initialize(version_base="1.1", config_path="../hydra_config"):
         cfg = compose(
             config_name="default",
             overrides=[
                 f"io.input.parquet_path={test_dir}/data/target.parquet",
                 "io.input.id_col_name=SMILES",
-                f"minimisation@local_min={min_method}",
-                f"minimisation@global_min={min_method}",
+                f"calculator={min_method}",
+                "optimiser@local_optimiser=bfgs",
+                "optimiser@global_optimiser=bfgs",
                 f"energy_eval={eval_method}",
                 "conformers.numConfs=1",
             ],
@@ -31,18 +32,18 @@ def test_strain_relief(min_method: str, eval_method: str):
 @pytest.mark.integration
 @pytest.mark.gpu
 def test_strain_relief_w_mace():
-    with initialize(version_base="1.1", config_path="../src/strain_relief/hydra_config"):
+    with initialize(version_base="1.1", config_path="../hydra_config"):
         cfg = compose(
             config_name="default",
             overrides=[
                 f"io.input.parquet_path={test_dir}/data/target.parquet",
                 "io.input.id_col_name=SMILES",
-                "minimisation@local_min=mace",
-                "minimisation@global_min=mace",
+                "optimiser@local_optimiser=bfgs",
+                "optimiser@global_optimiser=bfgs",
+                "calculator=mace",
                 "local_min.fmax=0.50",
-                "model=mace",
-                f"local_min.model_paths={test_dir}/models/MACE.model",
-                f"global_min.model_paths={test_dir}/models/MACE.model",
+                f"local_optimiser.model_paths={test_dir}/models/MACE.model",
+                f"global_optimiser.model_paths={test_dir}/models/MACE.model",
                 f"model.model_paths={test_dir}/models/MACE.model",
                 "conformers.numConfs=1",
             ],
@@ -56,18 +57,18 @@ def test_strain_relief_w_mace():
 @pytest.mark.integration
 @pytest.mark.gpu
 def test_strain_relief_w_esen(esen_model_path: str):
-    with initialize(version_base="1.1", config_path="../src/strain_relief/hydra_config"):
+    with initialize(version_base="1.1", config_path="../hydra_config"):
         cfg = compose(
             config_name="default",
             overrides=[
                 f"io.input.parquet_path={test_dir}/data/target.parquet",
                 "io.input.id_col_name=SMILES",
-                "minimisation@local_min=fairchem",
-                "minimisation@global_min=fairchem",
-                "local_min.fmax=0.50",
-                "model=fairchem",
-                f"local_min.model_paths={test_dir}/models/eSEN.pt",
-                f"global_min.model_paths={test_dir}/models/eSEN.pt",
+                "optimiser@local_optimiser=bfgs",
+                "optimiser@global_optimiser=bfgs",
+                "calculator=fairchem",
+                "local_optimiser.fmax=0.50",
+                f"local_optimiser.model_paths={test_dir}/models/eSEN.pt",
+                f"global_optimiser.model_paths={test_dir}/models/eSEN.pt",
                 f"model.model_paths={test_dir}/models/eSEN.pt",
                 "conformers.numConfs=1",
             ],
@@ -80,14 +81,15 @@ def test_strain_relief_w_esen(esen_model_path: str):
 
 @pytest.mark.integration
 def test_strain_relief_all_charged():
-    with initialize(version_base="1.1", config_path="../src/strain_relief/hydra_config"):
+    with initialize(version_base="1.1", config_path="../hydra_config"):
         cfg = compose(
             config_name="default",
             overrides=[
                 f"io.input.parquet_path={test_dir}/data/all_charged.parquet",
                 "io.input.id_col_name=id",
-                "minimisation@local_min=mmff94s",
-                "minimisation@global_min=mmff94s",
+                "optimiser@local_optimiser=bfgs",
+                "optimiser@global_optimiser=bfgs",
+                "calculator=mace",
                 "conformers.numConfs=1",
             ],
         )
