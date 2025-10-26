c28bcee605586891bf3e2e581566ebe4d6d93e0e
Ewan Wallace <ewan.wallace@roche.com>
2025-10-26 18:08:20 +0000

    updated typing

--- src/strain_relief/cmdline/_strain_relief.py
+++ src/strain_relief/cmdline/_strain_relief.py
@@ -7,7 +7,7 @@ from strain_relief.io import load_parquet
 
 
 @hydra.main(version_base=None, config_path="../../../hydra_config", config_name="default")
-def main(cfg: DictConfig) -> pd.DataFrame | None:
+def main(cfg: DictConfig) -> pd.DataFrame:
     """Main function to compute strain relief from a hydra config."""
     df = load_parquet(**cfg.io.input)
     return compute_strain(df=df, cfg=cfg)
