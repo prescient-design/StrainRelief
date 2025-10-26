81917312b6768c44a470c221f0924cb4ec538341
Ewan Wallace <ewan.wallace@roche.com>
2025-10-26 18:29:09 +0000

    absolute paths

--- src/strain_relief/cmdline/_strain_relief.py
+++ src/strain_relief/cmdline/_strain_relief.py
@@ -2,11 +2,11 @@ import hydra
 import pandas as pd
 from omegaconf import DictConfig
 
-from strain_relief import compute_strain
+from strain_relief import compute_strain, project_dir
 from strain_relief.io import load_parquet
 
 
-@hydra.main(version_base=None, config_path="../../../hydra_config", config_name="default")
+@hydra.main(version_base=None, config_path=str(project_dir / "hydra_config"), config_name="default")
 def main(cfg: DictConfig) -> pd.DataFrame:
     """Main function to compute strain relief from a hydra config."""
     df = load_parquet(**cfg.io.input)
