309c1b65a632c6b31896d0fd5f63c41e75186669
Ewan Wallace <ewan.wallace@roche.com>
2025-11-02 22:27:20 +0000

    updated for new config

--- examples/example_scripts.sh
+++ examples/example_scripts.sh
@@ -6,7 +6,7 @@
 #SBATCH --gpus-per-node 1
 #SBATCH --mem 10GB
 
-# script should be run from the StrainRelief root directory
+# scripts should be run from the StrainRelief root directory
 
 source ~/StrainRelief/.venv/bin/activate  # with uv
 # mamba activate strain  # with conda/mamba
@@ -19,32 +19,34 @@ source ~/StrainRelief/.venv/bin/activate  # with uv
 # This is the simplest and fastest implementation of StrainRelief using MMFF94s
 # and a minimial example dataset.
 strain-relief \
-    io.input.parquet_path=../data/example_ligboundconf_input.parquet \
-    io.output.parquet_path=../data/example_ligboundconf_output.parquet \
-    minimisation@global_min=mmff94s \
-    minimisation@local_min=mmff94s
+    io.input.parquet_path=./data/example_ligboundconf_input.parquet \
+    io.output.parquet_path=./data/example_ligboundconf_output.parquet \
+    optimiser@local_optimiser=bfgs \
+    optimiser@global_optimiser=bfgs \
+    calculator=mmff94s \
+    device=cpu
 
 # This script demonstrates using different force fields for minimisation
 # (MMFF94s) and energy evaluations (MACE).
 strain-relief \
-    io.input.parquet_path=../data/example_ligboundconf_input.parquet \
-    io.output.parquet_path=../data/example_ligboundconf_output.parquet \
-    minimisation@global_min=mmff94s \
-    minimisation@local_min=mmff94s \
-    energy_eval=mace \
-    model=mace \
-    model.model_paths=s3://prescient-data-dev/strain_relief/models/MACE.model
+    io.input.parquet_path=./data/example_ligboundconf_input.parquet \
+    io.output.parquet_path=./data/example_ligboundconf_output.parquet \
+    optimiser@local_optimiser=bfgs \
+    optimiser@global_optimiser=bfgs \
+    calculator=mmff94s \
+    energy_evaluation.calculator=mace \
+    energy_evaluation.calculator.model_paths=./tests/models/MACE.model \
+    device=cpu
 
 # This is the script as used for most calculations in the StrainRelief paper.
 # MACE is used for minimisation (and energy evalutions implicitly). A looser
-# convergence criteria is used for local minimisation. Note: a gpu is required
-# by default to run calculations with MACE.
+# convergence criteria is used for local minimisation.
 strain-relief \
     io.input.parquet_path=../data/example_ligboundconf_input.parquet \
     io.output.parquet_path=../data/example_ligboundconf_output.parquet \
-    minimisation@global_min=mace \
-    minimisation@local_min=mace \
-    local_min.fmax=0.50 \
-    model=mace \
-    model.model_paths=s3://prescient-data-dev/strain_relief/models/MACE.model \
-    hydra.verbose=true
+    optimiser@local_optimiser=bfgs \
+    optimiser@global_optimiser=bfgs \
+    calculator=mace \
+    calculator.model_paths=./tests/models/MACE.model \
+    hydra.verbose=true \
+    device=cuda
