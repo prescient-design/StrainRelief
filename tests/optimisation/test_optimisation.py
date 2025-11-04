e2ce56d18d410201c458feef6447b24ece17177a
Ewan Wallace <ewan.wallace@roche.com>
2025-11-04 11:02:49 +0000

    work in progress

--- tests/optimisation/test_optimisation.py
+++ tests/optimisation/test_optimisation.py
@@ -1,6 +1,21 @@
-import pytest
+from neural_optimiser.conformers import ConformerBatch
+from neural_optimiser.optimisers import BFGS
+from strain_relief.optimisation._optimisation import run_optimisation
 
 
-@pytest.mark.skip(reason="Test not implemented")
-def test_run_optimisation():
-    pass
+def test_run_optimisation_no_batching(optimiser: BFGS, batch: ConformerBatch) -> ConformerBatch:
+    """Test running optimisation without batching."""
+    out = run_optimisation(conformers=batch, optimiser=optimiser, batch_size=1, num_workers=0)
+
+    assert isinstance(out, ConformerBatch)
+    assert out.n_conformers == batch.n_conformers
+    assert out.n_atoms == batch.n_atoms
+
+
+def test_run_optimisation_batching(optimiser: BFGS, batch: ConformerBatch) -> ConformerBatch:
+    """Test running optimisation with batching."""
+    out = run_optimisation(conformers=batch, optimiser=optimiser, batch_size=3, num_workers=0)
+
+    assert isinstance(out, ConformerBatch)
+    assert out.n_conformers == batch.n_conformers
+    assert out.n_atoms == batch.n_atoms
