04a349e9dfc1777f205dc57dc0b5eb705d9e8171
Ewan Wallace <ewan.wallace@roche.com>
2025-11-26 15:54:10 +0000

    device arg

--- tests/optimisation/test_optimisation.py
+++ tests/optimisation/test_optimisation.py
@@ -3,18 +3,24 @@ from neural_optimiser.optimisers import BFGS
 from strain_relief.optimisation._optimisation import run_optimisation
 
 
-def test_run_optimisation_no_batching(opt: BFGS, batch: ConformerBatch) -> ConformerBatch:
+def test_run_optimisation_no_batching(
+    opt: BFGS, batch: ConformerBatch, device: str
+) -> ConformerBatch:
     """Test running optimisation without batching."""
-    out = run_optimisation(conformers=batch, optimiser=opt, batch_size=1, num_workers=0)
+    out = run_optimisation(
+        conformers=batch, optimiser=opt, batch_size=1, num_workers=0, device=device
+    )
 
     assert isinstance(out, ConformerBatch)
     assert out.n_conformers == batch.n_conformers
     assert out.n_atoms == batch.n_atoms
 
 
-def test_run_optimisation_batching(opt: BFGS, batch: ConformerBatch) -> ConformerBatch:
+def test_run_optimisation_batching(opt: BFGS, batch: ConformerBatch, device: str) -> ConformerBatch:
     """Test running optimisation with batching."""
-    out = run_optimisation(conformers=batch, optimiser=opt, batch_size=3, num_workers=0)
+    out = run_optimisation(
+        conformers=batch, optimiser=opt, batch_size=3, num_workers=0, device=device
+    )
 
     assert isinstance(out, ConformerBatch)
     assert out.n_conformers == batch.n_conformers
